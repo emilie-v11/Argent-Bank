6afbb273d43cbaee24d8bdc875402e8a0a219e08
Vukovic Emilie <emilievukovic79@gmail.com>
2021-12-22 12:47:18 +0100

    Add Header + Footer

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainNav from './MainNav/MainNav'
+
+const Header = () => {
+    return (
+        <header>
+            <MainNav />
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Header/MainNav/MainNav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../../assets/img/argentBankLogo.png';
+
+const MainNav = () => {
+    return (
+        <nav className="main-nav">
+            <NavLink className="main-nav-logo" to="/">
+                <img className="main-nav-logo-image" src={Logo} alt="Argent Bank Logo" />
+                <h1 className="sr-only">Argent Bank</h1>
+            </NavLink>
+            <div>
+                <NavLink className="main-nav-item" to="/login">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </NavLink>
+                {/* <button class="sign-in-button">Sign In</button> */}
+            </div>
+        </nav>
+    );
+};
+
+export default MainNav;

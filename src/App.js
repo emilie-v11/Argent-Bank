705876e9cb3b37dfa794fb12add872344b7f70cb
Vukovic Emilie <emilievukovic79@gmail.com>
2021-12-22 13:11:47 +0100

    Add BrowserRouter, Route, Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import Header from './components/Header/Header';
+import HomePage from './pages/HomePage/HomePage';
+import LogInPage from './pages/LogInPage/LogInPage';
+import UserPage from './pages/UserPage/UserPage';
+import Error404 from './pages/Error404/Error404';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <div className="App">
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LogInPage />} />
+                <Route path="/user" element={<UserPage />} />
+                <Route path="*" element={<Error404 />} />
+            </Routes>
+            <Footer />
         </div>
     );
 }

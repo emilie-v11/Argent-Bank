2a2b5e04e2193c7e790ed8217a0080901aace54c
Vukovic Emilie <emilievukovic79@gmail.com>
2021-12-22 16:12:40 +0100

    GitHub Issue Template : home-page-feature.md Completed
    - [X] Navigate to the home page (`/`)
    - [X] See all placeholder data from mockup

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HomePage.css'
+import Features from '../../components/Features/Features';
+import Hero from '../../components/Hero/Hero';
+
+const HomePage = () => {
+    return (
+        <main className="main-homepage">
+            <Hero />
+            <Features />
+        </main>
+    );
+};
+
+export default HomePage;

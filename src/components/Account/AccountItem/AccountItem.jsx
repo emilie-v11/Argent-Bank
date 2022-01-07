3cdde9618c5a43400d090f6ba5581a93e8741348
Vukovic Emilie <emilievukovic79@gmail.com>
2022-01-07 19:15:53 +0100

    Clean code

--- src/components/Account/AccountItem/AccountItem.jsx
+++ src/components/Account/AccountItem/AccountItem.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import './Account.css';
 
 const AccountItem = ({ id, title, amount, description }) => {
     return (
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './Features.css';
 import FeatureItem from '../../components/Features/FeatureItem/FeatureItem';
-// import ChatIcon from '../../assets/img/icon-chat.png';
-// import MoneyIcon from '../../assets/img/icon-money.png';
-// import SecurityIcon from '../../assets/img/icon-security.png';
 import { featureData } from '../../data/mockData';
 
-// TODO make mock
 const Features = () => {
     return (
         <section className="features">
